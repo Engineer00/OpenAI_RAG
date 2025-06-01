844c50df0c7c8c055dbfa73fbe04a6dbba239577
AH <ah@medacles.com>
2025-06-01 23:12:44 +0500

    Track frontend build directory for audio recorder component and update .gitignore

---
+++ streamlit_audio_recorder/st_audiorec/frontend/build/precache-manifest.39867572fce1f99835ede0c7d96379bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347fd0c12a38807c2d79df0fe4f6663b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a0c6855ce8225580adf",
+    "url": "./static/css/2.bfbf028b.chunk.css"
+  },
+  {
+    "revision": "0a0c6855ce8225580adf",
+    "url": "./static/js/2.e850748f.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.e850748f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ce61f2c0cb3d3f4393c",
+    "url": "./static/js/main.d5ad5553.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
